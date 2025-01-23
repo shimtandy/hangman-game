5cf60eef4368fd1e67dcafab0abd3d97321ed7eb
Liam <liam.alan.smth@gmail.com>
2025-01-23 19:39:40 +0000

    Add NavBar component and roughly style instructions page

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router";
+import styles from './NavBar.module.css'
+import backIcon from '/images/icon-back.svg'
+
+export default function NavBar({ title }) {
+    return (
+        <nav className={styles.navBar + " gradientText borderText"}>
+            <Link className={styles.backButton + ' buttonTwo'} to='/'><img src={backIcon}/></Link>
+            <h1>{title}</h1>
+        </nav>
+    )
+}
--- src/views/instructions/Instructions.jsx
+++ src/views/instructions/Instructions.jsx
@@ -1,33 +1,34 @@
-import { Link } from "react-router"
+import NavBar from "../../components/navBar/NavBar.jsx"
+import styles from './Instructions.module.css'
 
 function Instructions() {
     return (
-        <>
-        <nav>
-            <Link to='/'>Back</Link>
-            <h1>How to Play</h1>
-        </nav>
+        <div className={styles.layout}>
+            <NavBar title='How to Play' />
 
-        <main>
-            <ol>
-                <li>
-                    <div>01</div>
-                    <h2>First, choose a word category, like animals or movies. 
-                        The computer then randomly selects a secret word from that topic and shows you blanks for each letter of the word.</h2>
-                </li>
-                <li>
-                    <div>02</div>
-                    <h2>Take turns guessing letters. The computer fills in the relevant blank spaces if your guess is correct.
-                         If it's wrong, you lose some health, which empties after eight incorrect guesses.</h2>
-                </li>
-                <li>
-                    <div>03</div>
-                    <h2>You win by guessing all the letters in the word before your health runs out. 
-                        If the health bar empties before you guess the word, you lose.</h2>
-                </li>
-            </ol>
-        </main>
-        </>
+            <main>
+                <ol className={styles.instructionList}>
+                    <li>
+                        <div className={styles.instructionNum}>01</div>
+                        <h2 className={styles.instructionHeading}>Choose a category</h2>
+                        <p className={styles.instructionBody}>First, choose a word category, like animals or movies. 
+                            The computer then randomly selects a secret word from that topic and shows you blanks for each letter of the word.</p>
+                    </li>
+                    <li>
+                        <div className={styles.instructionNum}>02</div>
+                        <h2 className={styles.instructionHeading}>Guess letters</h2>
+                        <p className={styles.instructionBody}>Take turns guessing letters. The computer fills in the relevant blank spaces if your guess is correct.
+                            If it's wrong, you lose some health, which empties after eight incorrect guesses.</p>
+                    </li>
+                    <li>
+                        <div className={styles.instructionNum}>03</div>
+                        <h2 className={styles.instructionHeading}>Win or lose</h2>
+                        <p className={styles.instructionBody}>You win by guessing all the letters in the word before your health runs out. 
+                            If the health bar empties before you guess the word, you lose.</p>
+                    </li>
+                </ol>
+            </main>
+        </div>
     )
 }
 
